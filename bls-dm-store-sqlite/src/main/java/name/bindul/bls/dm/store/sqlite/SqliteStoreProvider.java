cdf6c6bc5a4728245260fb7c87baffcbc41658bf
Bindul Bhowmik <bindulbhowmik@gmail.com>
2025-11-24 00:22:22 -0700

    WIP - Can bring up a UI. And some service discovery

---
+++ bls-dm-store-sqlite/src/main/java/name/bindul/bls/dm/store/sqlite/SqliteStoreProvider.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2025. Bindul Bhowmik
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package name.bindul.bls.dm.store.sqlite;
+
+import java.io.IOException;
+
+import name.bindul.bls.dm.core.store.Store;
+import name.bindul.bls.dm.core.store.StoreProvider;
+
+public class SqliteStoreProvider extends StoreProvider {
+
+	@Override
+	public Store open(String location, String username, String password) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Store create(String location, String username, String password) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
