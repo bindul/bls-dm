c6e7c1db17edaecd752a128c23dd7a6542de621f
Bindul Bhowmik <bindulbhowmik@gmail.com>
2025-11-24 20:41:15 -0700

    WIP: JavaFX UI?

---
+++ bls-dm-ui-javafx/src/main/java/name/bindul/bls/dm/ui/jfx/BlsDmApp.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2025. Bindul Bhowmik
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package name.bindul.bls.dm.ui.jfx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class BlsDmApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		final FXMLLoader fxmlLoader = new FXMLLoader(BlsDmApp.class.getResource("/bls-dm-main-view.fxml"));
+		final Scene scene = new Scene(fxmlLoader.load());
+//		fxmlLoader.getController();
+				
+		primaryStage.setTitle("Bowling League Stats - Data Manager");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		Application.launch();
+	}
+}
---
+++ bls-dm-ui-javafx/src/main/java/name/bindul/bls/dm/ui/jfx/BlsDmAppController.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2025. Bindul Bhowmik
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package name.bindul.bls.dm.ui.jfx;
+
+import org.controlsfx.control.StatusBar;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.TabPane;
+
+public class BlsDmAppController {
+
+	@FXML
+	private TabPane tabContentContainer;
+	
+	@FXML
+	private StatusBar statusBar;
+	
+	@FXML
+	private Menu recentFilesMenu;
+	
+	@FXML
+	protected void handleNew() {
+		// TODO Implement
+	}
+	
+	@FXML
+	protected void handleOpen() {
+		// TODO Implement
+	}
+	
+	@FXML
+	protected void openBowlingCenters() {
+		// TODO Implement
+	}
+	
+	@FXML
+	protected void exportBlsWebsiteFormatData() {
+		// TODO Implement
+	}
+	
+	@FXML
+	protected void showAbout() {
+		// TODO Implement
+	}
+}
